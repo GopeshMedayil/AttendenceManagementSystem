ec70a0314d6e9e1321190ccae16c5a8e78a6ba81
bibin323@gmail.com <bibin323@gmail.com>
2016-10-06 00:02:13 +0530

    project

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$serverName = "167.114.14.136\sqlexpress"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"test_android", "UID"=>"elixirapp", "PWD"=>"newpassword@123#");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
